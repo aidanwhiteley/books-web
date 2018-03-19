023939a2360ae587c7366a7418ec41cc91802308
Aidan Whiteley <github@aidanwhiteley.com>
2018-03-19 17:51:09 +0000

    Typos

--- app/env.js
+++ app/env.js
@@ -32,11 +32,12 @@
     window.__env.logoutEndPoint = "http://localhost:8080/secure/api/logout";
     
     // The URL for app metrics
-    // Can be relative path unless using CORS. Th etrailing slash is important
+    // Can be relative path unless using CORS. The trailing slash is important
     window.__env.appMetrics = "http://localhost:8080/actuator/";
     
     // Are we using CORS - normally development only. 
-    // If you set this to true there is no XSRF enabled so beware!
+    // If you set this to true there is no XSRF enabled so beware.
+    // Use of CORS on production not recommended - see https://github.com/aidanwhiteley/books
     window.__env.useCORS = true;
     
     // *************************************************************
@@ -44,19 +45,19 @@
     // *************************************************************
     
     // The book club name
-    window.__env.applicationName = 'The Cloudy Book Club';
+    window.__env.applicationName = 'The Something Book Club';
     
     // The book club scope
-    window.__env.bookClubmembersScope = 'you know Aidan or Liz or live in Cockwood';
+    window.__env.bookClubmembersScope = 'you know <names of people running book club here>';
     
     // The book club admin / signup email
-    window.__env.bookClubAdminEmail = 'signup@cloudybookclub.com';
+    window.__env.bookClubAdminEmail = 'example@example.com';
     
     // The book club admin contact email
-    window.__env.bookClubContactEmail = 'contact@cloudybookclub.com';
+    window.__env.bookClubContactEmail = 'example@example.com';
     
     // Welcome / intro text
-    window.__env.welcomeText = 'Welcome to The Cloudy Book Club!';
+    window.__env.welcomeText = 'Welcome to The Something Book Club!';
     window.__env.welcomeBackText = 'Welcome back. Your last logon was on';
     window.__env.tagLineText = 'Books we\'ve been reading';
     
